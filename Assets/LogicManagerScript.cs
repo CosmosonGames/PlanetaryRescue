669e15d7a40de943cd1bec32f5f806d2eb19f05a
osun24 <114966159+osun24@users.noreply.github.com>
2023-04-29 21:01:21 -0700

    init

---
+++ Assets/LogicManagerScript.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LogicManagerScript : MonoBehaviour
+{
+    //load storyline scene
+    public void startGame()
+    {
+        SceneManager.LoadScene("IntroScene");
+    }
+
+    //get leaderboard from database, display it, and show the leaderboard scene
+    public void openLeaderboard()
+    {
+        //get leaderboard here...
+        SceneManager.LoadScene("LeaderboardScene");
+    }
+}
